6b8de1d51da359c95a77ae3ec8f929e9caf969e9
Ronald Ferguson <r0nn1ef8580@gmail.com>
2016-06-03 16:48:23 -0500

    added basic output

--- src/Controller/ApodController.php
+++ src/Controller/ApodController.php
@@ -13,7 +13,12 @@ use Drupal\Core\Datetime\DrupalDateTime;
 
 class ApodController extends ControllerBase {
 
+  const ONE_DAY = 86400;
+
   public function content($date=NULL) {
+    // First Astronomy Picture of the day appears to be July 1, 1995.
+    $first_image = DrupalDateTime::createFromTimestamp( mktime(0, 0, 0, 7, 1, 1995) );
+    $today = DrupalDateTime::createFromTimestamp( mktime(0, 0, 0, date('m'), date('j'), date('Y')) );
 
     if ( !empty($date) ) {
       if ( is_int($date) ) {
@@ -23,17 +28,45 @@ class ApodController extends ControllerBase {
       } else {
         $date = NULL;
       }
+    } else {
+      $date = $today;
     }
 
     $service = \Drupal::service('apod.service');
 
     $image = $service->getImage($date, TRUE);
 
+    $build = array();
+    
 
 
-    return array(
+    $build['image'] = array(
       '#type' => 'markup',
       '#markup' => '<pre>' . print_r($image, TRUE) . '</pre>',
     );
+
+    if ( $date->format('U') > $first_image->format('U') ) {
+      $previous_date = DrupalDateTime::createFromTimestamp( $date->format('U') - self::ONE_DAY );
+      $path = 'astronomy-picture-of-the-day/' . $previous_date->format('Y-m-d');
+      $build['prev_link'] = array(
+        '#theme' => 'link',
+        '#path' => $path,
+        '#alt' => '',
+        '#text' => $this->t('&laquo; Previous')
+      );
+    }
+
+    if ( $date->format('U') > $today->format('U') ) {
+      $next_date = DrupalDateTime::createFromTimestamp( $date->format('U') + self::ONE_DAY );
+      $path = 'astronomy-picture-of-the-day/' . $next_date->format('Y-m-d');
+      $build['next_link'] = array(
+        '#theme' => 'link',
+        '#path' => $path,
+        '#alt' => '',
+        '#text' => $this->t('Next &raquo;')
+      );
+    }
+
+    return $build;
   }
 }
